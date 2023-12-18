3ae1316e775349e0d01769c72d930378ba5429ab
obaydullahmhs <obaydullah@appscode.com>
2023-12-18 18:55:01 +0600

    Update init kafka-connector docker

--- kafka-connect-init/mongodb/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-MONGO_CONNECTOR_VERSION=1.11.0
-
-wget -P /opt/kafka/libs/connector-plugins https://repo1.maven.org/maven2/org/mongodb/kafka/mongo-kafka-connect/${MONGO_CONNECTOR_VERSION}/mongo-kafka-connect-${MONGO_CONNECTOR_VERSION}-all.jar
-
-echo "Downloading MongoDB Kafka Connector version ${MONGO_CONNECTOR_VERSION} to /opt/kafka/libs/connector-plugins"
